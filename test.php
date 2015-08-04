e0d3046041ccfe13c701a5e90cbefdce220ff5f5
Ajeyarajap <Ajeyarajap@users.noreply.github.com>
2015-08-04 11:24:50 +0530

    Create test.php

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+// When you are debugging or refactoring you could have to switch betwenn two versions
+// Of the same code. This trick allow you to keep the two versions in the same file and
+// use one or the other by just adding or removing a slash on the first comment.
+//*
+    echo "A";
+/*/
